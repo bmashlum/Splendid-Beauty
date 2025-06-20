0170100e47806462f8cb680b2831b176951e87d9
bmashlum <32199817+bmashlum@users.noreply.github.com>
2025-06-19 21:20:44 -0400

    Initial commit
    Project scaffolding and initial file setup.

--- components/google-maps.tsx
+++ components/google-maps.tsx
@@ -3,7 +3,9 @@
 
 export default function GoogleMaps() {
     // Consider making the embed URL an environment variable or prop if it might change
-    const mapEmbedUrl = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3312.8390!2d-83.9001!3d33.8390!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5a3c1c1c1c1c1%3A0x1c1c1c1c1c1c1c1c!2s104%20Covington%20St%2C%20Loganville%2C%20GA%2030052!5e0!3m2!1sen!2sus!4v1234567890!5m2!1sen!2sus";
+    // Use a proper Google Maps embed URL for the actual location
+    // To get this URL: Go to Google Maps, search for your location, click Share > Embed a map
+    const mapEmbedUrl = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3312.7690764470657!2d-83.90228492408!3d33.87891917322315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5b8f3a7b9c3d7%3A0x5e8f4a8b7e3f4a8b!2s104%20Covington%20St%2C%20Loganville%2C%20GA%2030052!5e0!3m2!1sen!2sus!4v1700000000000!5m2!1sen!2sus";
 
     return (
         <div className="w-full h-full rounded-lg overflow-hidden">
